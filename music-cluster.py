5a63e4c4e5707d8b106bd6f8a7a3e79695bf7631
Christian Böndgen <c.boendgen@gmail.com>
2025-12-10 18:05:18 +0100

    init marimo notebook

---
+++ music-cluster.py
@@ -0,0 +1,17 @@
+import marimo
+
+__generated_with = "0.18.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import pandas as pd
+    import numpy as np
+    import librosa
+    return
+
+
+if __name__ == "__main__":
+    app.run()
