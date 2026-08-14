6ea761e4af9b059722c556668089035ed02adcd3
Christian Böndgen <c.boendgen@gmail.com>
2026-08-14 09:57:18 +0200

    Update Names for Docker

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -2,4 +2,4 @@ source ../.env
 
 docker build --tag tensorflow-miniconda:latest .
 
-docker run -it --name sov --gpus all -p 2718:2718 -v ${PWD}/../:/app -v ${DRIVE}:/data tensorflow-miniconda
+docker run -it --name sov --gpus all -p 2718:2718 -v ${PWD}/../:/app -v ${WORK_PATH}:/data tensorflow-miniconda
